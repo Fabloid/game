837ef3f7c776c4471a639f531bf630357425dc40
Александр Ильин <alex_ili@mail.ru>
2018-07-30 22:27:17 +0700

    исправил проверку победителя

--- game/game/Controllers/HomeController.cs
+++ game/game/Controllers/HomeController.cs
@@ -18,47 +18,43 @@ namespace game.Controllers
         public ActionResult Index(int id=0)
         {
             char[] s = new char[9] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-            bool is_end;
             if (id!=0)
             {
-                is_end = (bool)Session["end"];
-                if (!is_end)
-                {
-                    s = (char[])Session["press"];
+                s = (char[])Session["press"];
+                string res = pc.Result_game(s);
+                if (res==" ")
+                {                    
                     if (s[id - 1] == ' ')
                     {
                         data.Add_move(id, 1);
                         s[id - 1] = 'X';
-
                         s = pc.Move_PC(s);
-                        ViewBag.Result = pc.Result_game(s);
-                        if (ViewBag.Result != " ")
-                        {
-                            Session["end"] = true;
-                            int win = 0;
-                            if (ViewBag.Result == "Вы победили") win = 1;
-                            if (ViewBag.Result == "Победил ИИ") win = 2;
-                            if (ViewBag.Result == "Ничья") win = 3;
-                            data.Save_result_game(win);
-                        }
                     }
-                        ViewBag.Symbol1 = s[0];
-                        ViewBag.Symbol2 = s[1];
-                        ViewBag.Symbol3 = s[2];
-                        ViewBag.Symbol4 = s[3];
-                        ViewBag.Symbol5 = s[4];
-                        ViewBag.Symbol6 = s[5];
-                        ViewBag.Symbol7 = s[6];
-                        ViewBag.Symbol8 = s[7];
-                        ViewBag.Symbol9 = s[8];
-                        Session["press"] = s;
                 }
+                else
+                {
+                    int win = 0;
+                    if (res == "Вы победили") win = 1;
+                    if (res == "Победил ИИ") win = 2;
+                    if (res == "Ничья") win = 3;
+                    data.Save_result_game(win);
+                }
+                ViewBag.Result = pc.Result_game(s);
+                ViewBag.Symbol1 = s[0];
+                ViewBag.Symbol2 = s[1];
+                ViewBag.Symbol3 = s[2];
+                ViewBag.Symbol4 = s[3];
+                ViewBag.Symbol5 = s[4];
+                ViewBag.Symbol6 = s[5];
+                ViewBag.Symbol7 = s[6];
+                ViewBag.Symbol8 = s[7];
+                ViewBag.Symbol9 = s[8];
+                Session["press"] = s;
             }
             else
             {
                 data.Add_game();
                 Session["press"] = s;
-                Session["end"] = false;
             }
             return View();
         }
